b2c76d07de0addb64ae27e27e4de7dbb494cc102
Ashley Davis <ashley@codecapers.com.au>
2023-07-28 09:32:20 +1000

    Fixes to the test.

--- example-2/src/index.test.js
+++ example-2/src/index.test.js
@@ -51,13 +51,9 @@ describe("metadata microservice", () => {
 
     const { startMicroservice } = require("./index"); 
 
-    //
-    // Tests go here.
-    //
-    
     test("microservice starts web server on startup", async () => {
         
-        await startMicroservice();
+        await startMicroservice("dbhost", "dbname", 3000);
 
         expect(mockListenFn.mock.calls.length).toEqual(1);     // Check only 1 call to 'listen'.
         expect(mockListenFn.mock.calls[0][0]).toEqual(3000);   // Check for port 3000.
@@ -65,7 +61,7 @@ describe("metadata microservice", () => {
 
     test("/videos route is handled", async () => {
         
-        await startMicroservice();
+        await startMicroservice("dbhost", "dbname", 3000);
 
         expect(mockGetFn).toHaveBeenCalled();
 
@@ -75,7 +71,7 @@ describe("metadata microservice", () => {
 
     test("/videos route retreives data via videos collection", async () => {
 
-        await startMicroservice();
+        await startMicroservice("dbhost", "dbname", 3000);
 
         const mockRequest = {};
         const mockJsonFn = jest.fn();
